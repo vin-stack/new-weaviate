0a9629550f5b17671d11aad69dcffe136466d479
MR.CHAY <84063946+vin-stack@users.noreply.github.com>
2024-05-31 18:42:28 +0530

    Update celery.py

--- main/celery.py
+++ main/celery.py
@@ -1,15 +1,9 @@
 import os
 from celery import Celery
-from .tasks import add_master_vectors_task # type: ignore
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 app = Celery('main')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Set the broker URL to use Redis
 app.conf.broker_url = os.environ.get('REDIS_URL')
-
-app.autodiscover_tasks()
-# add the tasks module explicitly
-app.register_tasks(add_master_vectors_task)
+app.conf.result_backend = os.environ.get('REDIS_URL')
+app.conf.task_routes = {
+    'main.tasks.add_master_vectors_task': {'queue': 'long_tasks'},
+}
