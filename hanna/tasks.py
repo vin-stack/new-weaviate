835a3deb5bfd4b135be820c32a76e7ef99b293d8
MR.CHAY <84063946+vin-stack@users.noreply.github.com>
2024-05-31 18:48:52 +0530

    Update tasks.py

--- hanna/tasks.py
+++ hanna/tasks.py
@@ -1,27 +1,22 @@
-from celery import shared_task
-from .models import Company
-import json
-import os
-import uuid
-from weaviate_client import WeaviateClient
+from celery import task
+from .master_vectors.MV import MasterVectors
+mv = MasterVectors()
 
-mv = WeaviateClient()
-
-@shared_task
-def add_master_vectors_task(company_data):
+@task()
+def add_master_vectors_task(company):
+    """
+    A Celery task that adds master vectors to the Weaviate database
+    """
     try:
-        company_data = json.loads(company_data)
-        company = Company.objects.get(collection=company_data['collection'])
+        if mv.weaviate_client.schema.exists(company['collection']) is False:
+            raise Exception('This collection does not exist!')
 
-        if not company:
-            return Response({'error': 'This collection does not exist!'}, status=status.HTTP_400_BAD_REQUEST)
+        documents = mv.slice_document.chunk_corpus(company['text'])
 
-        documents = slice_document.chunk_corpus(company_data['text'])
+        uid = mv.add_batch(documents, company['filename'], company['type'], company['collection'])
 
-        uid = mv.add_batch(documents, company_data['filename'], company_data['type'], company_data['collection'])
-
-        return Response({'msg': str(uid)}, status=status.HTTP_200_OK)
+        return {'msg': str(uid)}
     except Exception as e:
         print("TASK:")
         print(e)
-        return Response({'error': 'Something went wrong!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        raise
