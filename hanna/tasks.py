e057da4a7a092c6db0f6c4a8e4bd8b0b7c6c5c9e
MR.CHAY <84063946+vin-stack@users.noreply.github.com>
2024-05-31 15:37:01 +0530

    Create tasks.py

---
+++ hanna/tasks.py
@@ -0,0 +1,22 @@
+from celery import task
+from .master_vectors.MV import MasterVectors
+mv = MasterVectors()
+
+@task()
+def add_master_vectors_task(company):
+    """
+    A Celery task that adds master vectors to the Weaviate database
+    """
+    try:
+        if mv.weaviate_client.schema.exists(company['collection']) is False:
+            raise Exception('This collection does not exist!')
+
+        documents = mv.slice_document.chunk_corpus(company['text'])
+
+        uid = mv.add_batch(documents, company['filename'], company['type'], company['collection'])
+
+        return {'msg': str(uid)}
+    except Exception as e:
+        print("TASK:")
+        print(e)
+        raise
