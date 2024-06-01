41027ffcda8075d4062243afb78c59cc3201d12f
MR.CHAY <84063946+vin-stack@users.noreply.github.com>
2024-06-01 17:27:11 +0530

    Update credentials.py

--- hanna/credentials.py
+++ hanna/credentials.py
@@ -8,13 +8,14 @@ load_dotenv()
 username = "root"
 password = "CZLTVzJz-PgmQ-jttFcU9k"
 
+
 class ClientCredentials:
 
     def __init__(self):
         self.cohere_client = cohere.Client(settings.COHERE_API_KEY)
-        self.__auth_config = weaviate.auth.AuthApiKey(api_key=settings.WEAVIATE_API_KEY)
+        __auth_config = "Basic " + base64.b64encode(f"{username}:{password}".encode()).decode()
         self.weaviate_client = weaviate.Client(
-            url=settings.WEAVIATE_URL,
-            additional_headers={"X-Cohere-Api-Key": settings.COHERE_API_KEY},
-            auth_client_secret=self.__auth_config
+            url="https://weaviate-n2ppa-u16782.vm.elestio.app/",
+            additional_headers={"Authorization": __auth_config, "X-Cohere-Api-Key": settings.COHERE_API_KEY},
+
         )
